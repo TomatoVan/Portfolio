512af0c95ed3a5d80225bfdfb61ec7aa8a468b90
Leo <example@gmail.com>
2021-01-14 17:03:35 +0300

    first commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu'),
-    close = document.querySelector('.menu__close');
+      menu = document.querySelector('.menu'),
+      close = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click',() => {
     menu.classList.add('active');
@@ -8,4 +8,12 @@ hamburger.addEventListener('click',() => {
 
 close.addEventListener('click',() => {
     menu.classList.remove('active');
+});
+
+
+const num = document.querySelectorAll('.skills_item2-num'),
+      lines = document.querySelectorAll('.skills_item2-bar span');
+
+num.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
