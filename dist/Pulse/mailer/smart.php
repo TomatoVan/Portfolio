7a6366d0a77041c431f0a2fa6223d1686ee68642
Leo <example@gmail.com>
2021-02-06 18:14:04 +0300

    first commit

--- dist/Pulse/mailer/smart.php
+++ dist/Pulse/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'kovzel.leo@gmail.com';                 // Наш логин
-$mail->Password = 'zxzxzx12345';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
